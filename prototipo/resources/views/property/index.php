cfd8488bf0dac8d0683a50edcf9e19f803d5b06d
derek-alves <derek.alves@fatec.sp.gov.br>
2020-09-09 17:43:52 -0300

    Initial commit

---
+++ prototipo/resources/views/property/index.php
@@ -0,0 +1,3 @@
+<h1>Listagem de Imoveis</h1>
+
+<p><a href="<?= url('/imoveis/novo');?>">Cadastrar novo imóvel</a></p>
---
+++ prototipo/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/imoveis','App\Http\Controllers\PropertyController@index');
+
+Route::get('/imoveis/novo','App\Http\Controllers\PropertyController@create');
